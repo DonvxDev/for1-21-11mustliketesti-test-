78b1a6b1ead889cf67362a24211598cdb2bd27d0
DonvxDev <Donvxdev@gmail.com>
2026-06-28 12:03:30 +0200

    chore: add For1.21.11MustLikeTestI/test. source files

---
+++ src/main/java/com/example/for1_21_11mustliketesti_test_/For1.21.11MustLikeTestI/test..java
@@ -0,0 +1,16 @@
+package com.example.for1_21_11mustliketesti_test_;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class For1.21.11MustLikeTestI/test. implements ModInitializer {
+    public static final String MOD_ID = "for1_21_11mustliketesti_test_";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("For1.21.11MustLikeTestI/test. loaded!");
+        // TODO: Register items, blocks, events, and commands here
+    }
+}
